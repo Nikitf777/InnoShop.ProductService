05686d598c1d8d2661226902d39f1657fc610960
Nikita Samusev <nikitf777@gmail.com>
2025-11-18 22:35:03 +0300

    Use uint for id instead of int

--- models/Product.cs
+++ models/Product.cs
@@ -2,7 +2,7 @@ namespace InnoShop.ProductService.Models;
 
 public class Product
 {
-	public int Id { get; set; }
+	public uint Id { get; set; }
 	public string Name { get; set; } = "";
 	public string Description { get; set; } = "";
 	public int Price { get; set; }
